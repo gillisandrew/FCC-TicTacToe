276c85fc15295814d0e98b9875ce81ba681f494f
Andrew Gillis <gillis.andrew@gmail.com>
2015-12-28 17:43:21 -0500

    add controls

--- tictactoe.js
+++ tictactoe.js
@@ -17,6 +17,13 @@ round = function (n) {
     this.squares.free = [0, 1, 2, 3, 4, 5, 6, 7, 8];
     this.squares.player = [];
     this.squares.cpu = [];
+    this.resetRound = function() {
+        $('.board').removeClass('is-o');
+        $('.square').removeClass('o');
+        $('.square').removeClass('x');
+        $('#cpuStart').removeClass('disabled');
+        currentRound = new round();
+    }
     this.checkOutcomes = function () {
         var choice1 = [];
         var choice2 = [];
@@ -35,6 +42,7 @@ round = function (n) {
             }
             if (cpuCount === 2 && playerCount === 0) {
                 console.log('CPU Win!');
+                this.roundEndTime = new Date();
                 console.log(this.squares.win[i]);
                 return odd[0];
             } else if (playerCount === 2 && cpuCount === 0) {
@@ -47,24 +55,27 @@ round = function (n) {
         console.log(choice1);
         console.log(choice2);
         if (choice1.length) {
+            console.log('choice1');
             return choice1[0];
         } else {
-            var ideal;
+            var ideal = -1;
             var nIdeal = 0;
             for (var i = 0; i <= 8; i++) {
                 var count = choice2.reduce(function (n, val) {
                     return n + (val === i);
                 }, 0);
-                if (count > nIdeal) {
+                if (nIdeal < count) {
                     ideal = i;
                 }
             }
-            if(ideal === undefined) {
+            if(ideal === -1) {
                 var choice3 = this.squares.free.filter(function(check) {
                     return typeof check === 'number';
                 });
+                console.log('choice3')
                 return choice3[Math.floor(Math.random() * choice3.length + 1) - 1];
             }else {
+                console.log('choice2')
                 return ideal;
             }
         }
@@ -78,6 +89,7 @@ round = function (n) {
                 cpuPick = ideal[Math.floor(Math.random() * ideal.length + 1) - 1];
                 break;
             case 2:
+                $('#cpuStart').addClass('disabled')
                 if (!this.squares.free[4]) {
                     var ideal = [0, 2, 6, 8];
                     cpuPick = ideal[Math.floor(Math.random() * ideal.length + 1) - 1];
@@ -86,13 +98,38 @@ round = function (n) {
                     cpuPick = 4;
                 }
                 break;
+            case 3:
+                if (typeof this.squares.free[4] !== 'number') {
+                    cpuPick = this.checkOutcomes();
+                    break;
+                }else {
+                    cpuPick = 4;
+                    break;
+                }
             default:
                 cpuPick = this.checkOutcomes();
                 break;
         }
         console.log(cpuPick);
         this.squares.free[cpuPick] = true;
-        $('.square[data-square=' + cpuPick + ']').addClass('o');
+        if(this.turn % 2 === 0) {
+            $('.square[data-square=' + cpuPick + ']').addClass('o');
+        }else {
+            $('.square[data-square=' + cpuPick + ']').addClass('x');
+        }
+        if(cpuPick === undefined || this.roundEndTime !== null || this.squares.player.length >= 4) {
+            if(this.roundEndTime === null) {
+                currentScore.draw++;
+            }else {
+                currentScore.loss++;
+            }
+            $('.score-board #loss').html(currentScore.loss);
+            $('.score-board #draw').html(currentScore.draw);
+            setTimeout(function() {
+                currentRound.resetRound()
+            }, 2000);
+            
+        }
         return cpuPick;
     };
     this.playerTurn = function (selection) {
@@ -101,9 +138,12 @@ round = function (n) {
             this.turn++;
             this.squares.free[index] = false;
             this.squares.player.push(selection);
-            $('.square[data-square=' + selection + ']').addClass('x');
+            if(this.turn % 2 === 0) {
+                $('.square[data-square=' + selection + ']').addClass('o');
+            }else {
+                $('.square[data-square=' + selection + ']').addClass('x');
+            }
             this.cpuTurn();
         }
-        console.log(this.squares.free)
     };
 }
